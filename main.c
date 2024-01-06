7d7eb831f37aa456cbfde6f2e075921c93e50f59
Muhammad Burhan Arshad <112951994+MuhammadBurhan123@users.noreply.github.com>
2024-01-06 11:00:43 +0000

    Update main.c

--- main.c
+++ main.c
@@ -3,22 +3,18 @@
 int main() {
     int n, first = 0, second = 1, next, i;
 
-    // Get user input for the number of terms
     printf("Enter the number of terms: ");
     scanf("%d", &n);
 
-    // Display the Fibonacci series
     printf("Fibonacci Series: ");
 
     for (i = 0; i < n; i++) {
         printf("%d", first);
 
-        // Calculate the next term in the series
         next = first + second;
         first = second;
         second = next;
 
-        // Print comma and space if it's not the last term
         if (i < n - 1) {
             printf(", ");
         }
